007d37f6c4fbade86ec997e210542aaa64e48391
Archit26k <76521139+Archit26k@users.noreply.github.com>
2021-05-24 19:28:47 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$visitors_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from ='info@archit26k.github.io';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+               "User Email: $visitors_email.\n".
+               "Subject: $subject.\n".
+               "User message: $message.\n";
+
+$to = 'archit26k@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .="Reply-To: $visitors_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+               
+?>
